2288a2c6570de94333a5207357e0c76437da23ca
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-06 17:08:46 +0200

    init | components

--- table/src/app/app.module.ts
+++ table/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InputComponent } from './component/input/input.component';
+import { ParentComponent } from './component/parent/parent.component';
+import { TableComponent } from './component/table/table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, InputComponent, ParentComponent, TableComponent],
+  imports: [BrowserModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
