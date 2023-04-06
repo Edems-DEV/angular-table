2454e035a2fbc7c290013d8c763beb9c1d2b6e3f
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-06 20:23:14 +0200

    ?

---
+++ table/src/app/_default/COMPLETE/table-complete/table-complete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-complete',
+  templateUrl: './table-complete.component.html',
+  styleUrls: ['./table-complete.component.scss']
+})
+export class TableCompleteComponent {
+
+}
