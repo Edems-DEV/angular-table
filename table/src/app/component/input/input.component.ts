c2f721aeaf8884a07a2e23a42402f9c26fdc1304
Edems <57250017+Edems-DEV@users.noreply.github.com>
2023-04-06 22:47:07 +0200

    🆕 Components (country only)

--- table/src/app/component/input/input.component.ts
+++ table/src/app/component/input/input.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, DecimalPipe, NgFor, NgIf } from '@angular/common';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Country } from '../../_default/COMPLETE/country';
+import { CountryService } from '../../_default/COMPLETE/country.service';
+import {
+  NgbdSortableHeader,
+  SortEvent,
+} from '../../_default/COMPLETE/sortable.directive';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbPaginationModule,
+  NgbTypeaheadModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
-
+  constructor(public service: CountryService) {}
 }
--- table/src/app/component/table/table.component.ts
+++ table/src/app/component/table/table.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, DecimalPipe, NgFor, NgIf } from '@angular/common';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Country } from '../../_default/COMPLETE/country';
+import { CountryService } from '../../_default/COMPLETE/country.service';
+import {
+  NgbdSortableHeader,
+  SortEvent,
+} from '../../_default/COMPLETE/sortable.directive';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbPaginationModule,
+  NgbTypeaheadModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent {
+  countries$: Observable<Country[]>;
+  total$: Observable<number>;
 
+  @ViewChildren(NgbdSortableHeader)
+  headers!: QueryList<NgbdSortableHeader>;
+
+  constructor(public service: CountryService) {
+    this.countries$ = service.countries$;
+    this.total$ = service.total$;
+  }
+
+  onSort({ column, direction }: SortEvent) {
+    // resetting other headers
+    this.headers.forEach((header) => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    this.service.sortColumn = column;
+    this.service.sortDirection = direction;
+  }
 }
